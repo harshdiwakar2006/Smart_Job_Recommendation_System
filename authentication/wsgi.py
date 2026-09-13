016d62aa83697cdf7436923dc55d659979430b13
Harsh Diwakar <harshdiwakar2006@gmail.com>
2026-09-13 16:53:28 +0530

    first commit

--- authentication/wsgi.py
+++ authentication/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'authentication.settings')
 
 application = get_wsgi_application()
+
+# Required by Vercel serverless function entrypoint
+app = application
