3082c1a26b77196059cbc1394c20048cc27b32c7
Harsh Diwakar <harshdiwakar2006@gmail.com>
2026-09-13 17:06:22 +0530

    Enforce LF line endings for build script

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-
-# Upgrade pip and install packages in Vercel's managed environment
-python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt --break-system-packages
-
-# Collect static files
 python3 manage.py collectstatic --noinput --clear
